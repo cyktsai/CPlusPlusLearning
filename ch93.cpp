7239c6f6108139d94b693c0c21c376ac4a96b8ec
cyktsai <godieme@gmail.com>
2017-07-05 22:05:04 +0800

    ch93udpate

--- ch93.cpp
+++ ch93.cpp
@@ -3,14 +3,23 @@
 #include <list>
 #include <deque>
 #include <string>
+#include <iterator>
 
 using namespace std;
 
+bool showContainer(iterator<void> first_iter);
 int main()
 {
 	vector<string> svec;
 	list<string> slist;
 
 	string spouse("Beth");
+	slist.insert(slist.begin(), spouse);
 	return 0;
+}
+
+bool showContainer(iterator<void> first_iter)
+{
+
+	return true;
 }
